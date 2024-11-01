6190ae16dbf4a53ef62bc6441e15d2c9d01b0606
kemuru <102478601+kemuru@users.noreply.github.com>
2024-11-01 05:09:39 +0100

    feat: add schemas for app dropdown, add arrowlink field

---
+++ cms-backend/src/api/navbar-apps-section/controllers/navbar-apps-section.ts
@@ -0,0 +1,7 @@
+/**
+ * navbar-apps-section controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::navbar-apps-section.navbar-apps-section');
---
+++ cms-backend/src/api/navbar-apps-section/services/navbar-apps-section.ts
@@ -0,0 +1,7 @@
+/**
+ * navbar-apps-section service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::navbar-apps-section.navbar-apps-section');
