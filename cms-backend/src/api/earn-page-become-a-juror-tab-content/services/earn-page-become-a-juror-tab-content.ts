c1e015fa1e678d6639c9a96e3bc34bfb537ab1fe
kemuru <102478601+kemuru@users.noreply.github.com>
2024-11-27 04:55:10 +0100

    feat: add schemas, new components

---
+++ cms-backend/src/api/earn-page-become-a-juror-tab-content/services/earn-page-become-a-juror-tab-content.ts
@@ -0,0 +1,9 @@
+/**
+ * earn-page-become-a-juror-tab-content service
+ */
+
+import { factories } from "@strapi/strapi";
+
+export default factories.createCoreService(
+  "api::earn-page-become-a-juror-tab-content.earn-page-become-a-juror-tab-content"
+);
---
+++ cms-backend/src/api/earn-page-hero/routes/earn-page-hero.ts
@@ -0,0 +1,7 @@
+/**
+ * earn-page-hero router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::earn-page-hero.earn-page-hero');
