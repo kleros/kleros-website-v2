b3475573fccc3ff9c2a00b50c7df5c997ef7340f
Harman-singh-waraich <Waraichharman68@gmail.com>
2025-01-13 17:11:40 +0530

    refactor(cms):r-and-d-types

--- cms-backend/src/api/r-and-d-page-fellowship-waitlist-section/controllers/r-and-d-page-fellowship-waitlist-section.ts
+++
@@ -1,7 +0,0 @@
-/**
- * r-and-d-page-fellowship-waitlist-section controller
- */
-
-import { factories } from '@strapi/strapi'
-
-export default factories.createCoreController('api::r-and-d-page-fellowship-waitlist-section.r-and-d-page-fellowship-waitlist-section');
