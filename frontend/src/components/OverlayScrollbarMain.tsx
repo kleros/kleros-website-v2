a0778d9104d565dfbc90f9b9ea7e0daf1fdf6a3e
alcercu <333aleix333@gmail.com>
2025-01-30 17:27:26 +0100

    feat(frontend): add overlay scrollbar to main element

---
+++ frontend/src/components/OverlayScrollbarMain.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import React from "react";
+
+import { OverlayScrollbarsComponent } from "overlayscrollbars-react";
+import "overlayscrollbars/overlayscrollbars.css";
+
+interface IOverlayScrollbarBody extends React.ComponentProps<"body"> {
+  children: React.ReactNode;
+}
+
+const OverlayScrollbarBody: React.FC<IOverlayScrollbarBody> = ({
+  children,
+  className,
+}) => (
+  <OverlayScrollbarsComponent
+    element="body"
+    {...{ className }}
+    defer
+    data-overlayscrollbars-initialize
+    options={{ scrollbars: { autoHide: "move" } }}
+  >
+    {children}
+  </OverlayScrollbarsComponent>
+);
+
+export default OverlayScrollbarBody;
