40dab29c8f03205c33ce41b54477b25a24298135
Harman-singh-waraich <Waraichharman68@gmail.com>
2025-02-03 11:59:36 +0530

    refactor(frontend): home-page-responsive

--- frontend/src/app/home/components/GetInTouch.tsx
+++ frontend/src/app/home/components/GetInTouch.tsx
@@ -13,14 +13,16 @@ const GetInTouch: React.FC = async () => {
   ).then((res) => res.homeGetInTouchSection);
 
   return (
-    <div className="bg-background-1 px-6 py-12 md:grid md:grid-cols-2">
+    <div className="bg-background-1 px-6 py-12 md:grid md:grid-cols-2 lg:px-32 lg:py-24">
       <div>
-        <h3 className="mb-8 text-xl font-medium lg:text-2xl">{title}</h3>
-        <p className="mb-6 lg:text-lg">{subtitle}</p>
+        <h3 className="mb-8 text-xl font-medium lg:mb-12 lg:text-3xl">
+          {title}
+        </h3>
+        <p className="mb-6 lg:mb-8 lg:text-lg">{subtitle}</p>
         <ExternalLink className="mb-16" text={link.text} url={link.link.url} />
       </div>
       <Image
-        className="mx-auto mb-20"
+        className="mx-auto mb-20 lg:ml-auto"
         alt="Kleros Logo"
         src={icon.url}
         width="296"
