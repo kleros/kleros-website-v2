635744604c08036a2ca73964f72ae88c3e6992f9
Harman-singh-waraich <Waraichharman68@gmail.com>
2025-02-04 13:37:40 +0530

    refactor(frontend): tab-component-refactor

--- frontend/src/components/ResearchDevelopment/TabSection/FellowshipTabContent/index.tsx
+++ frontend/src/components/ResearchDevelopment/TabSection/FellowshipTabContent/index.tsx
@@ -21,21 +21,23 @@ const FellowshipTabContent: React.FC<IFellowshipTabContent> = ({
   fellows,
 }) => {
   return (
-    <div className="flex flex-col gap-8 pb-12 pt-[88px] lg:py-24">
-      <h1 className="text-xl font-medium text-primary-text lg:text-3xl">
-        {header}
-      </h1>
-      <p className="text-secondary-text lg:text-lg">{subtitle}</p>
+    <div className="bg-background-2 px-6 lg:px-32">
+      <div className="flex flex-col gap-8 pb-12 pt-[88px] lg:py-24">
+        <h1 className="text-xl font-medium text-primary-text lg:text-3xl">
+          {header}
+        </h1>
+        <p className="text-secondary-text lg:text-lg">{subtitle}</p>
 
-      <h3 className="my-8 text-lg text-secondary-text lg:text-xl">
-        {testimonialsHeader}
-      </h3>
-      <Testimonials {...{ testimonials }} />
+        <h3 className="my-8 text-lg text-secondary-text lg:text-xl">
+          {testimonialsHeader}
+        </h3>
+        <Testimonials {...{ testimonials }} />
 
-      <h3 className="my-4 text-lg text-secondary-text lg:text-xl">
-        {fellowsHeader}
-      </h3>
-      <Fellows {...{ fellows }} />
+        <h3 className="my-4 text-lg text-secondary-text lg:text-xl">
+          {fellowsHeader}
+        </h3>
+        <Fellows {...{ fellows }} />
+      </div>
     </div>
   );
 };
