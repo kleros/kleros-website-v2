ac2b729c5ae4976b4030e1b08534115852c188e4
alcercu <333aleix333@gmail.com>
2025-02-05 18:55:04 +0100

    fix(frontend): limit navbar width

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -34,80 +34,85 @@ const Navbar: React.FC<INavbar> = ({ navbarData }) => {
   return (
     <header
       className={clsx(
-        "fixed left-0 right-0 top-0 z-50 flex h-20 w-full",
-        "items-center justify-between bg-black/35 px-2 py-2",
-        "text-base text-white backdrop-blur-md",
-        "md:px-16 xl:px-32",
+        "fixed left-0 right-0 top-0 z-50 h-20 w-full bg-black/35 px-2",
+        "backdrop-blur-md md:px-16 xl:px-32",
       )}
     >
-      <CustomLink
-        href="/home"
-        className="flex items-center"
-        onClick={() => toggleMenuOpen(false)}
+      <div
+        className={clsx(
+          "mx-auto flex h-full max-w-7xl items-center justify-between",
+          "text-base text-white",
+        )}
       >
-        <Image
-          alt="Kleros"
-          src={klerosLogo?.logo_svg.url}
-          width={184}
-          height={48}
-        />
-      </CustomLink>
-
-      <button
-        className="ml-auto block text-white lg:hidden"
-        onClick={toggleMenuOpen}
-      >
-        <HamburgerButton className="h-5 w-6" isOpen={menuOpen} />
-      </button>
-
-      <div className="hidden lg:flex">
-        <DesktopNavigation
-          {...{ pathname, navLinks, resourceSections, appsSection, socials }}
-        />
-      </div>
-
-      <div className="hidden items-center lg:flex">
-        <CustomLink href={navbarButton?.link.url}>
-          <Button className="text-background-2">
-            {navbarButton?.link.name}
-          </Button>
+        <CustomLink
+          href="/home"
+          className="flex items-center"
+          onClick={() => toggleMenuOpen(false)}
+        >
+          <Image
+            alt="Kleros"
+            src={klerosLogo?.logo_svg.url}
+            width={184}
+            height={48}
+          />
         </CustomLink>
-      </div>
 
-      <AnimatePresence>
-        {menuOpen ? (
-          <motion.div
-            className={clsx(
-              "fixed inset-0 top-20 z-40 h-[calc(100dvh-5rem)] bg-black/50",
-            )}
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            exit={{ opacity: 0 }}
-            onClick={() => toggleMenuOpen(false)}
-          >
+        <button
+          className="ml-auto block text-white lg:hidden"
+          onClick={toggleMenuOpen}
+        >
+          <HamburgerButton className="h-5 w-6" isOpen={menuOpen} />
+        </button>
+
+        <div className="hidden lg:flex">
+          <DesktopNavigation
+            {...{ pathname, navLinks, resourceSections, appsSection, socials }}
+          />
+        </div>
+
+        <div className="hidden items-center lg:flex">
+          <CustomLink href={navbarButton?.link.url}>
+            <Button className="text-background-2">
+              {navbarButton?.link.name}
+            </Button>
+          </CustomLink>
+        </div>
+
+        <AnimatePresence>
+          {menuOpen ? (
             <motion.div
-              className="absolute bottom-0 top-0"
-              initial={{ right: "-100%" }}
-              animate={{ right: 0 }}
-              exit={{ right: "-100%" }}
+              className={clsx(
+                "fixed inset-0 top-20 z-40 h-[calc(100dvh-5rem)] bg-black/50",
+              )}
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              exit={{ opacity: 0 }}
+              onClick={() => toggleMenuOpen(false)}
             >
-              <MobileMenu
-                {...{
-                  pathname,
-                  navLinks,
-                  resourceSections,
-                  appsSection,
-                  socials,
-                  navbarButton,
-                }}
-                className="h-full overflow-y-auto md:h-auto"
-                closeFn={() => toggleMenuOpen(false)}
-                onClick={(e) => e.stopPropagation()}
-              />
+              <motion.div
+                className="absolute bottom-0 top-0"
+                initial={{ right: "-100%" }}
+                animate={{ right: 0 }}
+                exit={{ right: "-100%" }}
+              >
+                <MobileMenu
+                  {...{
+                    pathname,
+                    navLinks,
+                    resourceSections,
+                    appsSection,
+                    socials,
+                    navbarButton,
+                  }}
+                  className="h-full overflow-y-auto md:h-auto"
+                  closeFn={() => toggleMenuOpen(false)}
+                  onClick={(e) => e.stopPropagation()}
+                />
+              </motion.div>
             </motion.div>
-          </motion.div>
-        ) : null}
-      </AnimatePresence>
+          ) : null}
+        </AnimatePresence>
+      </div>
     </header>
   );
 };
