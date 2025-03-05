052021da6aea369b6342494b2b83d3f87ccb7db2
Harman-singh-waraich <Waraichharman68@gmail.com>
2025-03-05 18:11:06 +0530

    chore(cms-backend): graphql-config

--- cms-backend/config/plugins.ts
+++ cms-backend/config/plugins.ts
@@ -1,1 +1,12 @@
-export default () => ({});
+export default {
+  graphql: {
+    config: {
+      endpoint: "/graphql",
+      shadowCRUD: true,
+      depthLimit: 10,
+      amountLimit: 100,
+      defaultLimit: 100,
+      maxLimit: 100,
+    },
+  },
+};
