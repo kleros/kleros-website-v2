b5d5949733e48b549c050b044d84c3e2482644c6
kokialgo <koki@kleros.io>
2025-03-31 17:22:36 -0300

    fix: s3 plugin for media upload

--- cms-backend/config/middlewares.ts
+++ cms-backend/config/middlewares.ts
@@ -1,12 +1,41 @@
 export default [
-  'strapi::logger',
-  'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
+  "strapi::logger",
+  "strapi::errors",
+  /* Replace 'strapi::security', with this snippet */
+  /* Beginning of snippet */
+  {
+    name: "strapi::security",
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          "connect-src": ["'self'", "https:"],
+          "img-src": [
+            "'self'",
+            "data:",
+            "blob:",
+            "dl.airtable.com",
+            "*.amazonaws.com",
+            ,
+          ],
+          "media-src": [
+            "'self'",
+            "data:",
+            "blob:",
+            "dl.airtable.com",
+            "*.amazonaws.com",
+            ,
+          ],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
+  "strapi::cors",
+  "strapi::poweredBy",
+  "strapi::query",
+  "strapi::body",
+  "strapi::session",
+  "strapi::favicon",
+  "strapi::public",
 ];
--- cms-backend/config/plugins.ts
+++ cms-backend/config/plugins.ts
@@ -15,21 +15,17 @@ export default ({ env }) => {
         provider: "aws-s3",
         providerOptions: {
           s3Options: {
-            accessKeyId: env("AWS_ACCESS_KEY_ID"),
-            secretAccessKey: env("AWS_ACCESS_SECRET"),
             region: env("AWS_REGION"),
             params: {
+              ACL: env("AWS_ACL", "public-read"),
+              signedUrlExpires: env("AWS_SIGNED_URL_EXPIRES", 15 * 60),
               Bucket: env("AWS_BUCKET_NAME"),
             },
-          },
-        },
-        // These parameters could solve issues with ACL public-read access — see [this issue](https://github.com/strapi/strapi/issues/5868) for details
-        actionOptions: {
-          upload: {
-            ACL: null,
-          },
-          uploadStream: {
-            ACL: null,
+            actionOptions: {
+              upload: {},
+              uploadStream: {},
+              delete: {},
+            },
           },
         },
       },
