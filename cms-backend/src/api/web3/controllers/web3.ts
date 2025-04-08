7a33ba30b03a166c7509e21932cc5c8f19be7570
alcercu <333aleix333@gmail.com>
2025-04-08 13:10:13 +0200

    feat(cms): add web3 sections

---
+++ cms-backend/src/api/web3/controllers/web3.ts
@@ -0,0 +1,7 @@
+/**
+ * web3 controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::web3.web3');
---
+++ cms-backend/src/api/web3/services/web3.ts
@@ -0,0 +1,7 @@
+/**
+ * web3 service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::web3.web3');
